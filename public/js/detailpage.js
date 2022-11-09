80ed9da2b3d1c79fa0788542aacc2658a5d7d409
surafel58 <surafelsentayehu58@gmail.com>
2022-11-09 10:28:08 +0300

    fixed some issues

--- public/js/detailpage.js
+++ public/js/detailpage.js
@@ -1,35 +1,43 @@
-// display image description when mouseover and zoom effect 
+// display image description when mouseover and zoom effect
 
 // on mouse click go to description page with adding the background image name on the query for the next page to get its
 
-//check the image size and if its height is 1280 increase the height of the container 
+//check the image size and if its height is 1280 increase the height of the container
 
 //make the link's "back to previous page" href = previous page
 
-for( let i=0; i<document.getElementsByClassName("ulcontainer").length; i++)
-{
-    let wrapper = document.getElementsByClassName("wrapper")[i];
-    let image_div = document.getElementsByClassName("image_div")[i];
-    let ulContainer= document.getElementsByClassName("ulcontainer")[i];
-    let ul = document.getElementsByClassName("description_list")[i];
-    ulContainer.addEventListener("mouseover", () => {
-        ul.style.animation = "list 0.5s ease forwards";
-        image_div.style.scale = "1.2";
-    });
+//change project area to aharic when lang is in amhracic
 
-    ulContainer.addEventListener("mouseleave", ()=>{
-        ul.style.visibility = "visible";
-        ul.style.animation = "list1 0.5s ease forwards";
-        image_div.style.scale = "1";
-    });
+for (
+  let i = 0;
+  i < document.getElementsByClassName("ulcontainer").length;
+  i++
+) {
+  let wrapper = document.getElementsByClassName("wrapper")[i];
+  let image_div = document.getElementsByClassName("image_div")[i];
+  let ulContainer = document.getElementsByClassName("ulcontainer")[i];
+  let ul = document.getElementsByClassName("description_list")[i];
+  ulContainer.addEventListener("mouseover", () => {
+    ul.style.animation = "list 0.5s ease forwards";
+    image_div.style.scale = "1.2";
+  });
 
-    let imagename = image_div.style.backgroundImage;
-    imagename = imagename.substring(5,imagename.length-2);
-    ulContainer.addEventListener("click", ()=>{
-      window.location.href = window.location.href.split("#")[0] + `/${image_div.dataset.id}` ;
-    });
+  ulContainer.addEventListener("mouseleave", () => {
+    ul.style.visibility = "visible";
+    ul.style.animation = "list1 0.5s ease forwards";
+    image_div.style.scale = "1";
+  });
 
-  let imageSrc = image_div.style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0];
+  let imagename = image_div.style.backgroundImage;
+  imagename = imagename.substring(5, imagename.length - 2);
+  ulContainer.addEventListener("click", () => {
+    window.location.href =
+      window.location.href.split("#")[0] + `/${image_div.dataset.id}`;
+  });
+
+  let imageSrc = image_div.style.backgroundImage
+    .replace(/url\((['"])?(.*?)\1\)/gi, "$2")
+    .split(",")[0];
   let image = new Image();
   image.src = imageSrc;
   // image.onload = function () {
@@ -39,9 +47,20 @@ for( let i=0; i<document.getElementsByClassName("ulcontainer").length; i++)
   // };
 }
 
-
-
 let b_homepage = document.querySelector("#b_homepage a");
-b_homepage.addEventListener('click', ()=>{
+b_homepage.addEventListener("click", () => {
   b_homepage.setAttribute("href", document.referrer);
 });
+
+if(localStorage.getItem('amhClicked') === 'true')
+{
+  document.querySelectorAll('.projectarea').forEach(item => {
+    item.innerHTML = 'የፕሮጀክት ስፋት: ';
+  })
+}
+else
+{
+  document.querySelectorAll('.projectarea').forEach(item => {
+    item.innerHTML = 'Project area: ';
+  })
+}
